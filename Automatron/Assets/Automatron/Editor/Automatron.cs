e7deecf61fbf1a7ba54386779f534343a8d6301a
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-08-21 13:28:44 +0200

    Ofc it wasn't updated before

--- Automatron/Assets/Automatron/Editor/Automatron.cs
+++ Automatron/Assets/Automatron/Editor/Automatron.cs
@@ -1,5 +1,12 @@
 ﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
+using UnityEditor;
+
+public class Automatron : EditorWindow {
+
+    [MenuItem( "Window/Automatron" )]
+    private static void Init() {
+        var wnd = GetWindow<Automatron>( "Automatron" );
+        wnd.minSize = new Vector2( 1280, 720 );
+        wnd.Show( true );
+    }
 }
