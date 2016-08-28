75f6eb322f267593a619db4d1e5893f68c4e2df8
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-08-28 17:44:12 +0200

    Loading Automations and Drawers + can create them

--- Automatron/Assets/Automatron/Editor/AutomatronEditor.cs
+++ Automatron/Assets/Automatron/Editor/AutomatronEditor.cs
@@ -1,39 +1,97 @@
 ﻿using UnityEngine;
 using UnityEditor;
-using System.Collections;
 using System.Collections.Generic;
+using TNRD.Editor.Core;
+using System;
+using System.Linq;
+using TNRD.Automatron.Drawers;
+using TNRD.Editor;
+using TNRD.Editor.Utilities;
 
-namespace Automatron {
+namespace TNRD.Automatron {
 
-    public class AutomatronEditor : EditorWindow {
+    public class AutomatronEditor : ExtendedWindow {
+
+        #region Automation Loading
+        private static Dictionary<string, Type> automations = new Dictionary<string, Type>();
+
+        [InitializeOnLoadMethod]
+        private static void LoadAutomations() {
+            var mush = from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                       from type in assembly.GetTypes()
+                       let attributes = type.GetCustomAttributes( typeof( AutomationAttribute ), true )
+                       where attributes != null && attributes.Length > 0
+                       select new { AutomationType = type, Attributes = attributes.Cast<AutomationAttribute>() };
+
+            foreach ( var item in mush ) {
+                var name = item.Attributes.ElementAt( 0 ).Name;
+                if ( !automations.ContainsKey( name ) ) {
+                    automations.Add( name, item.AutomationType );
+                }
+            }
+        }
+        #endregion
+
+        #region Drawer Loading
+        private static Dictionary<Type, Type> drawers = new Dictionary<Type, Type>();
+
+
+        [InitializeOnLoadMethod]
+        private static void LoadDrawers() {
+            var mush = from assembly in AppDomain.CurrentDomain.GetAssemblies()
+                       from type in assembly.GetTypes()
+                       let attributes = type.GetCustomAttributes( typeof( CustomDrawerAttribute ), true )
+                       where attributes != null && attributes.Length > 0
+                       select new { DrawerType = type, Attributes = attributes.Cast<CustomDrawerAttribute>() };
+
+            foreach ( var item in mush ) {
+                var type = item.Attributes.ElementAt( 0 ).Type;
+                if ( !drawers.ContainsKey( type ) ) {
+                    drawers.Add( type, item.DrawerType );
+                }
+            }
+        }
+
+        public static Type GetDrawerType( Type fieldType ) {
+            if ( drawers.ContainsKey( fieldType ) ) {
+                return drawers[fieldType];
+            } else {
+                return typeof( HelpDrawer );
+            }
+        }
+        #endregion
 
         [MenuItem( "Window/Automatron" )]
         private static void Init() {
-            var wnd = GetWindow<AutomatronEditor>( "Automatron" );
-            wnd.minSize = new Vector2( 1280, 720 );
+            var wnd = CreateEditor( "Automatron" );
+            wnd.minSize = new Vector2( 960, 540 );
             wnd.Show( true );
         }
 
-        void OnGUI() {
-            if ( GUILayout.Button( "Execute!" ) ) {
-                ExecutActions();
-            }
+        protected override void OnInitialize() {
+            WindowStyle = EWindowStyle.NoToolbarLight;
         }
 
-        private void ExecutActions() {
-            var list = new List<Automation>();
-            EditorCoroutine.Start( ExecuteActionsAsync( list ) );
-        }
-
-        private IEnumerator ExecuteActionsAsync( List<Automation> automations ) {
-            foreach ( var item in automations ) {
-                var routine = item.Execute();
-                while ( routine.MoveNext() ) {
-                    yield return routine.Current;
+        protected override void OnGUI() {
+            if ( Input.ButtonReleased( EMouseButton.Right ) ) {
+                var menu = GenericMenuBuilder.CreateMenu();
+                foreach ( var item in automations ) {
+                    menu.AddItem( item.Key, false, CreateAutomation, new object[] { Input.MousePosition, item.Value } );
                 }
-                yield return null;
+                menu.ShowAsContext();
             }
-            yield break;
+
+            Repaint();
+        }
+
+        private void CreateAutomation( object data ) {
+            var mpos = (Vector2)( data as object[] )[0];
+            var type = (Type)( data as object[] )[1];
+
+            var instance = (Automation)Activator.CreateInstance( type );
+            instance.Position = mpos;
+
+            AddControl( instance );
         }
     }
 }
