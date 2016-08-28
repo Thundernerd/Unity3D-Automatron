fdba9528e92c738bb949b59493a96afe8ec24e29
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-08-28 17:43:04 +0200

    Sorting order constants

---
+++ Automatron/Assets/Automatron/Editor/ESortingOrder.cs
@@ -0,0 +1,10 @@
+﻿namespace TNRD.Automatron {
+
+    public class ESortingOrder {
+        public const int Default = 0;
+        public const int Line = 100;
+        public const int Automation = 200;
+        public const int AutomationSelected = 300;
+        public const int LineNoEnd = 400;
+    }
+}
