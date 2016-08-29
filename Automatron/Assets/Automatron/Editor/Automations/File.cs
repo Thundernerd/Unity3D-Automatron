e2ad95e9ca079ef5c4e1e4ba5a186c6eac724831
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-08-30 00:32:06 +0200

    Added file and WWW automations

---
+++ Automatron/Assets/Automatron/Editor/Automations/File.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+
+namespace TNRD.Automatron.Automations {
+
+    [Automation( "File/Copy File or Directory" )]
+    public class FileCopy : Automation {
+
+        public string From;
+        public string To;
+
+        public override IEnumerator Execute() {
+            FileUtil.CopyFileOrDirectory( From, To );
+            yield break;
+        }
+    }
+
+    [Automation( "File/Copy File Or Directory" )]
+    public class FileDelete : Automation {
+
+        public string Path;
+        [ReadOnly]
+        public bool Result;
+
+        public override IEnumerator Execute() {
+            Result = FileUtil.DeleteFileOrDirectory( Path );
+            yield break;
+        }
+    }
+
+    [Automation( "File/Get Project Relative Path" )]
+    public class FileGetProjectRelativePath : Automation {
+
+        public string Path;
+        [ReadOnly]
+        public string Result;
+
+        public override IEnumerator Execute() {
+            Result = FileUtil.GetProjectRelativePath( Path );
+            yield break;
+        }
+    }
+
+    [Automation( "File/Get Unique Temp Path In Project" )]
+    public class FileGetUniqueTempPathInProject : Automation {
+
+        [ReadOnly]
+        public string Result;
+
+        public override IEnumerator Execute() {
+            Result = FileUtil.GetUniqueTempPathInProject();
+            yield break;
+        }
+    }
+
+    [Automation( "File/Move File Or Directory" )]
+    public class FileMoveFileOrDirectory : Automation {
+
+        public string From;
+        public string To;
+
+        public override IEnumerator Execute() {
+            FileUtil.MoveFileOrDirectory( From,To );
+            yield break;
+        }
+    }
+
+    [Automation( "File/Replace Directory" )]
+    public class FileReplaceDirectory : Automation {
+
+        public string Src;
+        public string Dst;
+
+        public override IEnumerator Execute() {
+            FileUtil.ReplaceDirectory( Src, Dst );
+            yield break;
+        }
+    }
+
+    [Automation( "File/Replace File" )]
+    public class FileReplaceFile : Automation {
+
+        public string Src;
+        public string Dst;
+
+        public override IEnumerator Execute() {
+            FileUtil.ReplaceFile( Src, Dst );
+            yield break;
+        }
+    }
+}
---
+++ Automatron/Assets/Automatron/Editor/Automations/WWW.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace TNRD.Automatron.Automations {
+
+    [Automation( "WWW/Audio Clip" )]
+    public class WWWAudioClip : Automation {
+
+        public string URL;
+        [ReadOnly]
+        public AudioClip Result;
+
+        public override IEnumerator Execute() {
+            var www = new WWW( URL );
+            while ( !www.isDone ) {
+                yield return www;
+                Progress = www.progress;
+            }
+            Result = www.audioClip;
+            yield break;
+        }
+    }
+
+    [Automation( "WWW/Movie Texture" )]
+    public class WWWMovie : Automation {
+
+        public string URL;
+        [ReadOnly]
+        public MovieTexture Result;
+
+        public override IEnumerator Execute() {
+            var www = new WWW( URL );
+            while ( !www.isDone ) {
+                yield return www;
+                Progress = www.progress;
+            }
+            Result = www.movie;
+            yield break;
+        }
+    }
+
+    [Automation( "WWW/Text" )]
+    public class WWWText : Automation {
+
+        public string URL;
+        [ReadOnly]
+        public string Result;
+
+        public override IEnumerator Execute() {
+            var www = new WWW( URL );
+            while ( !www.isDone ) {
+                yield return www;
+                Progress = www.progress;
+            }
+            Result = www.text;
+            yield break;
+        }
+    }
+
+    [Automation( "WWW/Texture" )]
+    public class WWWTexture : Automation {
+
+        public string URL;
+        [ReadOnly]
+        public Texture Result;
+
+        public override IEnumerator Execute() {
+            var www = new WWW( URL );
+            while ( !www.isDone ) {
+                yield return www;
+                Progress = www.progress;
+            }
+            Result = www.texture;
+            yield break;
+        }
+    }
+
+    [Automation( "WWW/Escape URL" )]
+    public class WWWEscape : Automation {
+
+        public string URL;
+        [ReadOnly]
+        public string Result;
+
+        public override IEnumerator Execute() {
+            Result = WWW.EscapeURL( URL );
+            yield break;
+        }
+    }
+
+    [Automation( "WWW/Unescape URL" )]
+    public class WWWUnEscapeURL : Automation {
+
+        public string URL;
+        [ReadOnly]
+        public string Result;
+
+        public override IEnumerator Execute() {
+            Result = WWW.UnEscapeURL( URL );
+            yield break;
+        }
+    }
+}
