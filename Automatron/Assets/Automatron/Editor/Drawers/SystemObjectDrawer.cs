3151876cccfa6fa69b4e2f28fd1ec565f201d765
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-08-29 16:20:21 +0200

    Placeholder drawer for System.object

---
+++ Automatron/Assets/Automatron/Editor/Drawers/SystemObjectDrawer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace TNRD.Automatron.Drawers {
+
+    [CustomDrawer( typeof( object ) )]
+    public class SystemObjectDrawer : AutomationDrawer {
+
+        public override void OnGUI( Rect rect, string name, ref object value ) {
+            base.OnGUI( rect, name, ref value );
+            EditorGUI.HelpBox( rect, string.Format( "Placeholder for field '{0}'", name ), MessageType.Info );
+        }
+    }
+}
