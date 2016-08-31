511570b874147fc41c8cb74a7c341ca27b592767
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-08-31 18:08:03 +0200

    Added Scene Drawers

---
+++ Automatron/Assets/Automatron/Editor/Drawers/EditorBuildSettingsSceneDrawer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using TNRD.Automatron;
+using UnityEngine.SceneManagement;
+
+namespace TNRD.Automatron.Drawers {
+
+    [CustomDrawer( typeof( EditorBuildSettingsScene ) )]
+    public class EditorBuildSettingsSceneDrawer : AutomationDrawer {
+
+        public override void OnGUI( Rect rect, string name, ref object value ) {
+            base.OnGUI( rect, name, ref value );
+            EditorGUI.BeginDisabledGroup( IsReadOnly );
+            EditorGUI.HandlePrefixLabel( rect, GetControlRect(), new GUIContent( name ) );
+            try {
+                var scene = (EditorBuildSettingsScene)value;
+                SceneAsset asset = null;
+
+                if ( scene != null && !string.IsNullOrEmpty( scene.path ) ) {
+                    asset = AssetDatabase.LoadAssetAtPath<SceneAsset>( scene.path );
+                }
+
+                var nAsset = EditorGUI.ObjectField( GetControlRect(), asset, typeof( SceneAsset ), true );
+                if ( nAsset != asset ) {
+                    if ( nAsset != null ) {
+                        value = Find( AssetDatabase.GetAssetOrScenePath( nAsset ) );
+                    } else {
+                        value = null;
+                    }
+                }
+
+            } catch ( ExitGUIException ) { }
+            EditorGUI.EndDisabledGroup();
+        }
+
+        private EditorBuildSettingsScene Find( string path ) {
+            return EditorBuildSettings.scenes.Where( s => s.path == path ).FirstOrDefault();
+        }
+    }
+}
---
+++ Automatron/Assets/Automatron/Editor/Drawers/SceneDrawer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace TNRD.Automatron.Drawers {
+
+    [CustomDrawer( typeof( Scene ) )]
+    public class SceneDrawer : AutomationDrawer {
+
+        public override void OnGUI( Rect rect, string name, ref object value ) {
+            base.OnGUI( rect, name, ref value );
+            EditorGUI.BeginDisabledGroup( IsReadOnly );
+            EditorGUI.HandlePrefixLabel( rect, GetControlRect(), new GUIContent( name ) );
+            try {
+                var scene = (Scene)value;
+                SceneAsset asset = null;
+
+                if ( !string.IsNullOrEmpty( scene.path ) ) {
+                    asset = AssetDatabase.LoadAssetAtPath<SceneAsset>( scene.path );
+                }
+
+                var nAsset = EditorGUI.ObjectField( GetControlRect(), asset, typeof( SceneAsset ), true );
+                if ( nAsset != null ) {
+                    if ( nAsset != asset ) {
+                        var path = AssetDatabase.GetAssetOrScenePath( nAsset );
+                        var nScene = SceneManager.GetSceneByPath( path );
+                        value = nScene;
+                    }
+                } else {
+                    value = null;
+                }
+            } catch ( ExitGUIException ) { }
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+}
