80fcae0df71a032b02788cbb9ab581e9bc3a4d87
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-09-01 12:55:25 +0200

    Updated GetName

--- Automatron/Assets/Automatron/Editor/Automation.cs
+++ Automatron/Assets/Automatron/Editor/Automation.cs
@@ -69,10 +69,14 @@ namespace TNRD.Automatron {
 
         private void GetName() {
             name = ( GetType().GetCustomAttributes( typeof( AutomationAttribute ), false )[0] as AutomationAttribute ).Name;
-            if ( name.Length > 35 ) {
+            if ( name.Contains( "/" ) && name.Length > 35 ) {
                 var index = name.IndexOf( '/' );
                 var lastIndex = name.LastIndexOf( '/' );
                 name = name.Remove( index, lastIndex - index );
+            }
+            if ( name.Contains( "/" ) && name.Length > 35 ) {
+                var index = name.LastIndexOf( '/' );
+                name = name.Substring( index + 1 );
             }
         }
 
