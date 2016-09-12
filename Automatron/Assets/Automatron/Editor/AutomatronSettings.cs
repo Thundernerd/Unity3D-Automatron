c3531fc60f3a856675ba820b86f0a35cb4dad111
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-09-12 17:26:01 +0200

    Namespace

--- Automatron/Assets/Automatron/Editor/AutomatronSettings.cs
+++ Automatron/Assets/Automatron/Editor/AutomatronSettings.cs
@@ -1,85 +1,88 @@
 ﻿using UnityEditor;
 using UnityEngine;
 
-public class AutomatronSettings {
+namespace TNRD.Automatron {
 
-    public abstract class EditorPrefsItem<T> {
-        public string Key;
-        public string Label;
-        public T DefaultValue;
+    public class AutomatronSettings {
 
-        public EditorPrefsItem( string key, string label, T defaultValue ) {
-            if ( string.IsNullOrEmpty( key ) ) {
-                throw new System.ArgumentNullException( "key" );
+        public abstract class EditorPrefsItem<T> {
+            public string Key;
+            public string Label;
+            public T DefaultValue;
+
+            public EditorPrefsItem( string key, string label, T defaultValue ) {
+                if ( string.IsNullOrEmpty( key ) ) {
+                    throw new System.ArgumentNullException( "key" );
+                }
+
+                Key = key;
+                Label = label;
+                DefaultValue = defaultValue;
             }
 
-            Key = key;
-            Label = label;
-            DefaultValue = defaultValue;
+            public abstract T Value { get; set; }
+            public abstract void Draw();
+
+            public static implicit operator T( EditorPrefsItem<T> s ) {
+                return s.Value;
+            }
         }
 
-        public abstract T Value { get; set; }
-        public abstract void Draw();
+        public class EditorPrefsInt : EditorPrefsItem<int> {
 
-        public static implicit operator T( EditorPrefsItem<T> s ) {
-            return s.Value;
-        }
-    }
+            public EditorPrefsInt( string key, string label, int defaultValue )
+                : base( key, label, defaultValue ) { }
 
-    public class EditorPrefsInt : EditorPrefsItem<int> {
+            public override int Value {
+                get { return EditorPrefs.GetInt( Key, DefaultValue ); }
+                set { EditorPrefs.SetInt( Key, value ); }
+            }
 
-        public EditorPrefsInt( string key, string label, int defaultValue )
-            : base( key, label, defaultValue ) { }
-
-        public override int Value {
-            get { return EditorPrefs.GetInt( Key, DefaultValue ); }
-            set { EditorPrefs.SetInt( Key, value ); }
+            public override void Draw() {
+                Value = EditorGUILayout.IntField( Label, Value );
+            }
         }
 
-        public override void Draw() {
-            Value = EditorGUILayout.IntField( Label, Value );
-        }
-    }
+        public class EditorPrefsString : EditorPrefsItem<string> {
 
-    public class EditorPrefsString : EditorPrefsItem<string> {
+            public EditorPrefsString( string key, string label, string defaultValue )
+                : base( key, label, defaultValue ) { }
 
-        public EditorPrefsString( string key, string label, string defaultValue )
-            : base( key, label, defaultValue ) { }
+            public override string Value {
+                get { return EditorPrefs.GetString( Key, DefaultValue ); }
+                set { EditorPrefs.SetString( Key, value ); }
+            }
 
-        public override string Value {
-            get { return EditorPrefs.GetString( Key, DefaultValue ); }
-            set { EditorPrefs.SetString( Key, value ); }
-        }
-
-        public override void Draw() {
-            Value = EditorGUILayout.TextField( Label, Value );
-        }
-    }
-
-    public class EditorPrefsBool : EditorPrefsItem<bool> {
-
-        public EditorPrefsBool( string key, string label, bool defaultValue )
-            : base( key, label, defaultValue ) { }
-
-        public override bool Value {
-            get { return EditorPrefs.GetBool( Key, DefaultValue ); }
-            set { EditorPrefs.SetBool( Key, value ); }
+            public override void Draw() {
+                Value = EditorGUILayout.TextField( Label, Value );
+            }
         }
 
-        public override void Draw() {
-            Value = EditorGUILayout.Toggle( Label, Value );
+        public class EditorPrefsBool : EditorPrefsItem<bool> {
+
+            public EditorPrefsBool( string key, string label, bool defaultValue )
+                : base( key, label, defaultValue ) { }
+
+            public override bool Value {
+                get { return EditorPrefs.GetBool( Key, DefaultValue ); }
+                set { EditorPrefs.SetBool( Key, value ); }
+            }
+
+            public override void Draw() {
+                Value = EditorGUILayout.Toggle( Label, Value );
+            }
         }
-    }
 
-    public static EditorPrefsString ConfigFolder = new EditorPrefsString( "TNRD.Automatron.ConfigFolder", "Config Folder", Application.dataPath + "/Automatron/Configs/" );
-    public static EditorPrefsBool AutoSave = new EditorPrefsBool( "TNRD.Automatron.AutoSave", "Auto Save", true );
+        public static EditorPrefsString ConfigFolder = new EditorPrefsString( "TNRD.Automatron.ConfigFolder", "Config Folder", Application.dataPath + "/Automatron/Configs/" );
+        public static EditorPrefsBool AutoSave = new EditorPrefsBool( "TNRD.Automatron.AutoSave", "Auto Save", true );
 
-    [PreferenceItem( "Automatron" )]
-    public static void PreferencesGUI() {
-        ConfigFolder.Draw();
-        AutoSave.Draw();
+        [PreferenceItem( "Automatron" )]
+        public static void PreferencesGUI() {
+            ConfigFolder.Draw();
+            AutoSave.Draw();
 
-        GUILayout.FlexibleSpace();
-        EditorGUILayout.LabelField( "Version 1.0", EditorStyles.centeredGreyMiniLabel );
+            GUILayout.FlexibleSpace();
+            EditorGUILayout.LabelField( "Version 1.0", EditorStyles.centeredGreyMiniLabel );
+        }
     }
 }
