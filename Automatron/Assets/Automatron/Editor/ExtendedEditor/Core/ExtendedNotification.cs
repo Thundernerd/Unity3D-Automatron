d549dadd7752ce650ec35b6fe6b4f2eb5f365380
Christiaan Bloemendaal <chrisbloemendaal@gmail.com>
2016-09-19 21:20:45 +0200

    Namespace

--- Automatron/Assets/Automatron/Editor/ExtendedEditor/Core/ExtendedNotification.cs
+++ Automatron/Assets/Automatron/Editor/ExtendedEditor/Core/ExtendedNotification.cs
@@ -1,81 +1,33 @@
 ﻿using System.Collections.Generic;
-using TNRD.Automatron.Editor.Core;
 using UnityEngine;
 
-public class ExtendedNotification : ExtendedControl {
+namespace TNRD.Automatron.Editor.Core {
 
-    private static List<ExtendedNotification> notifications = new List<ExtendedNotification>();
+    public class ExtendedNotification : ExtendedControl {
 
-    public GUIContent Content;
-    public Color Color;
-    public float Duration;
+        private static List<ExtendedNotification> notifications = new List<ExtendedNotification>();
 
-    private bool slideIn = false;
-    private bool movingOut = false;
-    private float speed = 600f;
-    private float outSpeed = 600f;
+        public GUIContent Content;
+        public Color Color;
+        public float Duration;
 
-    public ExtendedNotification() { }
+        private bool slideIn = false;
+        private bool movingOut = false;
+        private float speed = 600f;
+        private float outSpeed = 600f;
 
-    public ExtendedNotification( string text, bool isError ) {
-        Content = new GUIContent( text );
-        Color = isError ? Color.red : Color.white;
-        Duration = 1.25f;
-        AnchorPoint = TNRD.Automatron.Editor.EAnchor.TopLeft;
-    }
+        public ExtendedNotification() { }
 
-    protected override void OnInitialize() {
-        notifications.Add( this );
-
-        var y = 30f;
-        var n = new List<ExtendedNotification>( notifications );
-        foreach ( var item in n ) {
-            if ( item.ID == ID ) {
-                break;
-            } else {
-                y += item.Size.y;
-                y += 10f;
-            }
+        public ExtendedNotification( string text, bool isError ) {
+            Content = new GUIContent( text );
+            Color = isError ? Color.red : Color.white;
+            Duration = 1.25f;
+            AnchorPoint = TNRD.Automatron.Editor.EAnchor.TopLeft;
         }
 
-        Position = new Vector2( Window.Size.x + 25, y );
-    }
+        protected override void OnInitialize() {
+            notifications.Add( this );
 
-    protected override void OnInitializeGUI() {
-        ExtendedGUI.NotificationTextStyle.CalcMinMaxWidth( Content, out Size.y, out Size.x );
-        Size.y = ExtendedGUI.NotificationTextStyle.CalcHeight( Content, Size.x );
-        Size.x = Mathf.Max( 125, Size.x );
-
-        outSpeed = 600f * ( Size.x / 125 );
-    }
-
-    protected override void OnDestroy() {
-        notifications.Remove( this );
-    }
-
-    protected override void OnAfterSerialize() {
-        Remove();
-    }
-
-    protected override void OnUpdate() {
-        if ( !slideIn ) {
-            Position.x = Mathf.MoveTowards( Position.x, Window.Size.x - ( Size.x + 25 ), ExtendedEditor.DeltaTime * outSpeed );
-            if ( Position.x <= Window.Size.x - ( Size.x + 25 ) ) {
-                slideIn = true;
-            }
-        } else {
-            if ( Duration > 0 ) {
-                Duration -= ExtendedEditor.DeltaTime;
-            } else {
-                movingOut = true;
-                Position.y = Mathf.MoveTowards( Position.y, -( Size.y + 25 ), ExtendedEditor.DeltaTime * speed );
-                if ( Position.y <= -( Size.y + 25 ) ) {
-                    Remove();
-                }
-            }
-        }
-
-        if ( !movingOut ) {
             var y = 30f;
             var n = new List<ExtendedNotification>( notifications );
             foreach ( var item in n ) {
@@ -87,15 +39,65 @@ public class ExtendedNotification : ExtendedControl {
                 }
             }
 
-            Position.y = Mathf.MoveTowards( Position.y, y, ExtendedEditor.DeltaTime * speed );
+            Position = new Vector2( Window.Size.x + 25, y );
         }
-    }
 
-    protected override void OnGUI() {
-        GUI.Box( Rectangle, "", ExtendedGUI.NotificationBackgroundStyle );
-        var c = GUI.contentColor;
-        GUI.contentColor = Color;
-        GUI.Label( Rectangle, Content, ExtendedGUI.NotificationTextStyle );
-        GUI.contentColor = c;
+        protected override void OnInitializeGUI() {
+            ExtendedGUI.NotificationTextStyle.CalcMinMaxWidth( Content, out Size.y, out Size.x );
+            Size.y = ExtendedGUI.NotificationTextStyle.CalcHeight( Content, Size.x );
+            Size.x = Mathf.Max( 125, Size.x );
+
+            outSpeed = 600f * ( Size.x / 125 );
+        }
+
+        protected override void OnDestroy() {
+            notifications.Remove( this );
+        }
+
+        protected override void OnAfterSerialize() {
+            Remove();
+        }
+
+        protected override void OnUpdate() {
+            if ( !slideIn ) {
+                Position.x = Mathf.MoveTowards( Position.x, Window.Size.x - ( Size.x + 25 ), ExtendedEditor.DeltaTime * outSpeed );
+                if ( Position.x <= Window.Size.x - ( Size.x + 25 ) ) {
+                    slideIn = true;
+                }
+            } else {
+                if ( Duration > 0 ) {
+                    Duration -= ExtendedEditor.DeltaTime;
+                } else {
+                    movingOut = true;
+                    Position.y = Mathf.MoveTowards( Position.y, -( Size.y + 25 ), ExtendedEditor.DeltaTime * speed );
+                    if ( Position.y <= -( Size.y + 25 ) ) {
+                        Remove();
+                    }
+                }
+            }
+
+            if ( !movingOut ) {
+                var y = 30f;
+                var n = new List<ExtendedNotification>( notifications );
+                foreach ( var item in n ) {
+                    if ( item.ID == ID ) {
+                        break;
+                    } else {
+                        y += item.Size.y;
+                        y += 10f;
+                    }
+                }
+
+                Position.y = Mathf.MoveTowards( Position.y, y, ExtendedEditor.DeltaTime * speed );
+            }
+        }
+
+        protected override void OnGUI() {
+            GUI.Box( Rectangle, "", ExtendedGUI.NotificationBackgroundStyle );
+            var c = GUI.contentColor;
+            GUI.contentColor = Color;
+            GUI.Label( Rectangle, Content, ExtendedGUI.NotificationTextStyle );
+            GUI.contentColor = c;
+        }
     }
 }
